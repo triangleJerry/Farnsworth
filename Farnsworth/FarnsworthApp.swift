d07d4ea001afc2faaa13428cccaf66ca0c43736b
mongrong <eunseock47@gmail.com>
2025-05-23 21:36:59 +0900

    ChatView 추가

--- Farnsworth/FarnsworthApp.swift
+++ Farnsworth/FarnsworthApp.swift
@@ -25,7 +25,7 @@ struct FarnsworthApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ChatView()
         }
         .modelContainer(sharedModelContainer)
     }
